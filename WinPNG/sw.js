c5c7c205adc210d71d5624f2fd7e1087907af78e
harnenim <harne_@naver.com>
2024-03-04 16:36:45 +0900

    Add files via upload

--- WinPNG/sw.js
+++ WinPNG/sw.js
@@ -1,11 +1,27 @@
-self.addEventListener('install', (e) => {
-	e.waitUntil(
-		caches.open('ohli-store').then((cache) => cache.addAll([
-				'/WinPNG/Viewer.html'
-		]))
-    );
-});
+const VERSION = "2024.03.04.v1";
 
-self.addEventListener('fetch', (e) => {
-	e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
-});
+if (self.window) {
+	setTimeout(async function() {
+		if (navigator.serviceWorker) {
+			console.log(window.CURRENT_VERSION);
+			if (!window.CURRENT_VERSION || CURRENT_VERSION < VERSION) {
+				navigator.serviceWorker.register("sw.js?" + Math.random());
+			}
+		}
+	}, 10);
+	
+} else {
+	console.log(self);
+	self.addEventListener('install', (e) => {
+		console.log("install")
+		e.waitUntil(
+			caches.open('ohli-store').then((cache) => cache.addAll([
+					'/WinPNG/Viewer.html'
+			]))
+	    );
+	});
+	
+	self.addEventListener('fetch', (e) => {
+		e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+	});
+}
