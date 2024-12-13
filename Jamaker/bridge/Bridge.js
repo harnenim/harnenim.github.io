f57fb3b39e7d9d22ad84a0575663c1b4389ae585
harne_ <harne_@naver.com>
2024-12-13 19:57:27 +0900

    누락된 강제 버전업 추가

--- Jamaker/bridge/Bridge.js
+++ Jamaker/bridge/Bridge.js
@@ -93,7 +93,7 @@ function WebPlayerBridge() {
 					if (this.window && this.window.name) {
 						return;
 					}
-					this.window = window.open(location.href.substring(0, location.href.lastIndexOf("/")) + "/bridge/player.html?241205.2", "player", "scrollbars=no,location=no");
+					this.window = window.open(location.href.substring(0, location.href.lastIndexOf("/")) + "/bridge/player.html?241213", "player", "scrollbars=no,location=no");
 					if (this.window) {
 						if (this.window.document) {
 							this.window.document.title = "플레이어";
@@ -240,7 +240,7 @@ var player = {}; // 가상 플레이어
 		if (this.window && this.window.name) {
 			return;
 		}
-		this.window = window.open(location.href.split("index.html?241205.2")[0] + "view/player.html?241205.2", "player", "scrollbars=no,location=no");
+		this.window = window.open(location.href.split("index.html?241213")[0] + "view/player.html?241213", "player", "scrollbars=no,location=no");
 		if (this.window) {
 			this.window.document.title = "플레이어";
 		}
